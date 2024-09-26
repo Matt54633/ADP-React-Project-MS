127650b9ec672f65787da32805a5f535a79cedd0
Matt <69976995+Matt54633@users.noreply.github.com>
2024-09-26 11:56:06 +0100

    Remove select outline

--- front-end-app/src/components/SortSelect.jsx
+++ front-end-app/src/components/SortSelect.jsx
@@ -7,7 +7,7 @@ const SortSelect = (props) => {
     return (
         <>
             <select onChange={handleChange}
-                className="shadow-md ring-1 ring-gray-300 bg-white text-slate-700 text-sm font-bold flex items-center justify-center   h-8 rounded-lg">
+                className="shadow-md outline-none ring-1 ring-gray-300 bg-white text-slate-700 text-sm font-bold flex items-center justify-center   h-8 rounded-lg">
                 <option value="A-Z">A-Z</option>
                 <option value="Z-A">Z-A</option>
             </select>
